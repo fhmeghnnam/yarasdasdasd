be05757a1415b340db1b92372098548e0a4e45a8
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-08-18 14:56:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,13 +2,36 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "!";          //    Token Here
-//    Add emoji name
+<CLient>.on("messageReactionAdd", (reaction, user) => {
+  if (user.bot) return;
+  const member = reaction.message.member
+  switch (reaction.name) {
+    case "emoji_name_1":
+    member.addRole("612601847634591753").then((res) => {
+      
+     
+    }).catch(console.error);
+    break;
+  
+  };
+})
 
-//    Add role name
-client.on("messageReactionAdd", function(users) {
-users.addRole(users.guild.roles.find("id", 612601847634591753));
-users.removeRole(users.guild.roles.find("id", 612601847634591753));
-});
+<CLient>.on("messageReactionRemove", (reaction, user) => {
+  if (user.bot) return;
+  const member = reaction.message.member
+  switch (reaction.name) {
+    case "emoji_name_1":
+    member.removeRole("612601847634591753").then((res) => {
+      // You can do something like this, or nothing at all. Your choice.
+      reaction.message.channel.send(`You've been removed from the \`${res.name}\` role!`)
+    }).catch(console.error);
+    break;
+    case "emoji_name_2":
+    member.removeRole("someOtherRole").then((res) => {
+      
+    }).catch(console.error);
+  };
+})
 
 client.login(process.env.BOT_TOKEN);
 
