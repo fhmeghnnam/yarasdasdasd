7b1c989ef73e901db2d80fd851f5be1913748425
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-08-18 14:26:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,61 @@
 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "!";
+const token = "Token Here";              //    Token Here
+//    Add emoji name
+var emojiname = [":Yes:"];
 
-const Discord = require('discord.js'); // تعريف Discord.js
-const fs = require('fs'); // تعريف fs.
-const client = new Discord.Client(); // تعريف الكلينت
-const prefix = "$"; //تعريف البرفك
+//    Add role name
+var rolename=["Member"];
 
 
-client.on("ready", () => {
-  const channel = client.channels.get("445780572640509962");
-  if (!channel) return console.error("The channel does not exist!");
-  channel.join().then(connection => {
-    // Yay, it worked!
-    console.log("Successfully connected.");
-  }).catch(e => {
-    // Oh no, it errored! Let's log it to console :)
-    console.error(e);
-  });
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
+
+
+client.on('message', msg => {
+
+if(msg.content.startsWith(prefix+"reaction")){
+  if(!msg.channel.guild) return;
+  for(let n in emojiname){
+  var emoji =[msg.guild.emojis.find(r => r.name == emojiname[n])];
+  for(let i in emoji){
+   msg.react(emoji[i]);
+  }
+ }
+}
+});
+
+
+
+client.on("messageReactionAdd",(reaction,user)=>{
+  if(!user) return;
+  if(user.bot)return;
+  if(!reaction.message.channel.guild) return;
+  for(let n in emojiname){
+  if(reaction.emoji.name == emojiname[n]){
+    let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);          
+    reaction.message.guild.member(user).addRole(role).catch(console.error);
+  }
+}
+});
+
+
+client.on("messageReactionRemove",(reaction,user)=>{
+  if(!user) return;
+  if(user.bot)return;
+  if(!reaction.message.channel.guild) return;
+  for(let n in emojiname){
+  if(reaction.emoji.name == emojiname[n]){
+    let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);   
+    reaction.message.guild.member(user).removeRole(role).catch(console.error);
+  }
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
 
 client.login(process.env.BOT_TOKEN)
